ec778088dccee04be35ce8a26685802ff6422404
beliel <aminranjbar19462@gmail.com>
2024-05-29 13:33:07 +0330

    new upload

--- user_router.py
+++ user_router.py
@@ -1,47 +1,61 @@
 from fastapi import APIRouter, File, UploadFile, Depends, HTTPException, status
-from fastapi import APIRouter, HTTPException, status, Depends
 from mongo import connection
 from utills import jwt_tools
 from auth_repo import authentication
+import os
+import imghdr
 from model import FileResponse
 from minio_setup import upload_file_to_minio
-from mongo import connection
-import magic
+from datetime import datetime
+
+
 router = APIRouter(prefix='/user', tags=["User Api"])
 
 
 @router.post('/me')
-async def user_info(current_user=Depends
-                    (authentication.authenticate_token())):
-
+async def user_info(current_user=Depends(authentication.authenticate_token())):
     current_user.pop("password")
     return current_user
-
-mime = magic.Magic(mime=True)
 
 
 @router.post("/upload_profile_pic/", response_model=FileResponse)
 async def upload_file(file: UploadFile = File(...), current_user=Depends(authentication.authenticate_token())):
     try:
-        file_type = mime.from_buffer(file.file.read(2048))
-        if not file_type.startswith('image'):
+        content = await file.read()
+        file_type = imghdr.what(None, h=content)
+        await file.seek(0)
+
+        if file_type not in ["jpeg", "png", "gif"]:
             raise HTTPException(
-                status_code=400, detail="فقط فایل‌های عکس مجاز هستند.")
+                status_code=400, detail="Only image files are allowed (jpeg, png, gif).")
 
         minio_url = upload_file_to_minio(
             file.file, file.filename, file.content_type)
         download_url = f"/user/download/{file.filename}"
+        upload_time = datetime.utcnow()
 
-        connection.site_database.users.update_one(
+        user_data = connection.site_database.users.find_one_and_update(
             {"username": current_user["username"]},
             {"$set": {
                 "filename": file.filename,
                 "minio_url": minio_url,
-                "download_url": download_url
-            }})
+                "download_url": download_url,
+                "upload_time": upload_time,
+                "old_minio_url": minio_url,
+                "old_download_url": download_url
+            }},
+            return_document=True
+        )
 
-        return FileResponse(filename=file.filename, content_type=file.content_type, minio_url=minio_url, download_url=download_url)
+        return FileResponse(filename=file.filename,
+                            content_type=file.content_type,
+                            minio_url=minio_url, download_url=download_url,
+                            )
+
+    except HTTPException as e:
+        raise e
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"An error occurred while uploading the file: {str(e)}")
+            detail=f"An error occurred while uploading the file: {str(e)}"
+        )
