a65f1c6e0d53e2b36069b19a1b271ebbf8b0c4ba
beliel <aminranjbar19462@gmail.com>
2024-05-29 12:20:49 +0330

    new upload pic

--- user_router.py
+++ user_router.py
@@ -3,11 +3,10 @@ from fastapi import APIRouter, HTTPException, status, Depends
 from mongo import connection
 from utills import jwt_tools
 from auth_repo import authentication
-import os
 from model import FileResponse
 from minio_setup import upload_file_to_minio
 from mongo import connection
-
+import magic
 router = APIRouter(prefix='/user', tags=["User Api"])
 
 
@@ -18,19 +17,17 @@ async def user_info(current_user=Depends
     current_user.pop("password")
     return current_user
 
-
-router = APIRouter(prefix='/user', tags=["User Api"])
-
-
-@router.post('/me')
-async def user_info(current_user=Depends(authentication.authenticate_token())):
-    current_user.pop("password")
-    return current_user
+mime = magic.Magic(mime=True)
 
 
-@router.post("/uploadfile/", response_model=FileResponse)
+@router.post("/upload_profile_pic/", response_model=FileResponse)
 async def upload_file(file: UploadFile = File(...), current_user=Depends(authentication.authenticate_token())):
     try:
+        file_type = mime.from_buffer(file.file.read(2048))
+        if not file_type.startswith('image'):
+            raise HTTPException(
+                status_code=400, detail="فقط فایل‌های عکس مجاز هستند.")
+
         minio_url = upload_file_to_minio(
             file.file, file.filename, file.content_type)
         download_url = f"/user/download/{file.filename}"
@@ -46,4 +43,5 @@ async def upload_file(file: UploadFile = File(...), current_user=Depends(authent
         return FileResponse(filename=file.filename, content_type=file.content_type, minio_url=minio_url, download_url=download_url)
     except Exception as e:
         raise HTTPException(
-            status_code=500, detail=f"An error occurred while uploading the file: {str(e)}")
+            status_code=500,
+            detail=f"An error occurred while uploading the file: {str(e)}")
