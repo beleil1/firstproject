04cc7263f5c231b220ff1e1b0590ea7ec14430a5
beliel <aminranjbar19462@gmail.com>
2024-06-06 16:38:34 +0330

    message

--- api/message.py
+++ api/message.py
@@ -2,21 +2,31 @@ from fastapi import APIRouter, HTTPException, Depends, WebSocket, WebSocketDisco
 from models.model_message import Message
 from db.mongo import connection
 from schemas.auth_repo import authentication
-from bson import ObjectId
+from bson import ObjectId, errors, json_util
 from datetime import datetime
 from typing import List
 from services.wbconnection import ConnectionManager
-from models.model_message import Message
 
 router = APIRouter(prefix='/messages', tags=["Messages API"])
 manager = ConnectionManager()
 
 
+def validate_object_id(id: str):
+    try:
+        return ObjectId(id)
+    except errors.InvalidId:
+        raise HTTPException(
+            status_code=400, detail=f"{id} is not a valid ObjectId")
+
+
 @router.post('/')
 async def create_message(message: Message, current_user=Depends(authentication.authenticate_token())):
+    sender_id = validate_object_id(message.sender_id)
+    receiver_id = validate_object_id(message.receiver_id)
+
     message_data = {
-        "sender_id": ObjectId(message.sender_id),
-        "receiver_id": ObjectId(message.receiver_id),
+        "sender_id": sender_id,
+        "receiver_id": receiver_id,
         "content": message.content,
         "timestamp": datetime.now().isoformat()
     }
@@ -29,8 +39,8 @@ async def create_message(message: Message, current_user=Depends(authentication.a
 
 @router.get('/{friend_id}')
 async def get_messages(friend_id: str, current_user=Depends(authentication.authenticate_token())):
-    user_id = ObjectId(current_user['id'])
-    friend_id = ObjectId(friend_id)
+    user_id = validate_object_id(current_user['id'])
+    friend_id = validate_object_id(friend_id)
 
     messages = await connection.site_database.messages.find({
         "$or": [
@@ -41,8 +51,13 @@ async def get_messages(friend_id: str, current_user=Depends(authentication.authe
 
     if not messages:
         raise HTTPException(status_code=404, detail="No messages found")
+        messages_jason = json_util.dumps(messages)
+    if not messages:
+        messages_json = []
+    else:
+        messages_json = json_util.dumps(messages)
 
-    return messages
+    return messages_json
 
 
 @router.websocket("/ws/{current_user_id}/{friend_id}")
@@ -51,9 +66,11 @@ async def websocket_endpoint(websocket: WebSocket, current_user_id: str, friend_
     try:
         while True:
             message = await websocket.receive_text()
+            sender_id = validate_object_id(current_user_id)
+            receiver_id = validate_object_id(friend_id)
             message_data = {
-                "sender_id": ObjectId(current_user_id),
-                "receiver_id": ObjectId(friend_id),
+                "sender_id": sender_id,
+                "receiver_id": receiver_id,
                 "content": message,
                 "timestamp": datetime.now().isoformat()
             }
